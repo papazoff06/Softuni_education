dd85e6961ed2ecb87c46acc6d9a83578aa3a6923
Anton Papazov <134053962+papazoff06@users.noreply.github.com>
2023-09-26 15:23:26 +0300

    Delete data_types_and_variables_exerscise directory

--- data_types_and_variables_exerscise/chars _to_string.py
+++
@@ -1,4 +0,0 @@
-a = input()
-b = input()
-c = input()
-print(f'{a}{b}{c}')
--- data_types_and_variables_exerscise/integer_operations.py
+++
@@ -1,7 +0,0 @@
-number_1 = int(input())
-number_2 = int(input())
-number_3 = int(input())
-number_4 = int(input())
-
-decision = int((number_1 + number_2) / number_3) * number_4
-print(int(decision))
